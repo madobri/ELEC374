3083fcfcc510d8a332d997b19adefe3b9ba02b3b
Madison O'Brien <21maob@queensu.ca>
2025-02-02 21:36:27 -0500

    done

--- boothAlgorithm1b.v
+++ boothAlgorithm1b.v
@@ -1,27 +1,35 @@
 module boothAlgorithm1b(
+	//input M and Q
 	input signed [31:0] M,
 	input signed [31:0] Q,
 	
+	//output
 	output signed [63:0] Result
 	
 
 );
-
+	
+	//holds Q with sign extension
 	reg signed [33:0] mult;
-
+	
+	//holds A, sum of each computation
 	reg signed[63:0] A;
 	
+	//int i for loop
 	integer i;
 	
 	always @(*) begin
 		
+		//set A =0 as base case
 		A =0;
 		
+		//mult = Q with sign extension 0
 		mult = {Q,1'b0};
 		
 		
-		
+		//loop through each bit of mult, comparing to the rght bit to determine computation
 		for(i =0; i<32; i=i+1) begin
+			//case 2 bits in mult
 			case (mult[1:0])
 				2'b00: A=A;
 				2'b01: A= A+(M<<i);
@@ -31,7 +39,7 @@ module boothAlgorithm1b(
 			endcase 
 			
 			
-			
+			//logically shift right 1 bit
 			mult = $signed(mult)>>>1;
 			
 			
@@ -40,7 +48,8 @@ module boothAlgorithm1b(
 		end 
 		
 	end
-		
+	
+	//set result equal A	
 	assign Result = A;
 		
 endmodule
--- boothAlgorithm1b_tb.v
+++ boothAlgorithm1b_tb.v
@@ -2,30 +2,31 @@
 
 module boothAlgorithm1b_tb;
 
- 
-    reg signed[31:0] M, Q;
+
+	 
+	reg signed[31:0] M, Q;
 	
     
-    wire [63:0] Result;
+	wire [63:0] Result;
 
-    boothAlgorithm1b uut (
-        .M(M),
-		  .Q(Q),
-        .Result(Result)
+	boothAlgorithm1b uut (
+		.M(M),
+		.Q(Q),
+		.Result(Result)
     );
 
-    initial begin
+	initial begin
       
-        M = 32'b11111111111111111111111111001011;
-		  Q = 32'b11111111111111111111111111000010;
+		M = 32'b11111111111111111111111111001011;
+		Q = 32'b11111111111111111111111111000010;
 		  
-		  #10;
+		#10;
 		  
    
-        $display("Time=%0t | M=%b | Q=%b| Result=%b", $time, M, Q, Result);
+		$display("Time=%0t | M=%b | Q=%b| Result=%b", $time, M, Q, Result);
 
      
-    end
+		end
 
 endmodule
 
