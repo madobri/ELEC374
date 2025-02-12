ead33b6df56ccb28f58da94c4d9af31b6e7cc8e5
rachelmillar <147862467+rachelmillar@users.noreply.github.com>
2025-02-12 15:49:58 -0500

    Adder Done

--- ALUGOOD/adder.v
+++ ALUGOOD/adder.v
@@ -1,19 +1,20 @@
 module adder(A, B, Result);
 
-input [31:0] A, B;
-output reg [31:0] Result; 
+	input [31:0] A, B;
+	output reg[63:0] Result; 
 
-reg [32:0] LocalCarry;
+	reg [32:0] LocalCarry;
 
-integer i = 0;
+	integer i = 0;
 
-always@(A or B)
-	begin
-		LocalCarry = 33'd0;
-		for(i=0; i < 32; i = i+1)
+	always@(A or B)
 		begin
-			Result[i] = A[i]^B[i]^LocalCarry[i];
-			LocalCarry[i+1] = (A[i]&B[i])|(LocalCarry[i]&(A[i]|B[i]));
-		end
-end
+			LocalCarry = 33'd0;
+			for(i=0; i < 32; i = i+1)
+			begin
+				Result[i] = A[i]^B[i]^LocalCarry[i];
+				LocalCarry[i+1] = (A[i]&B[i])|(LocalCarry[i]&(A[i]|B[i]));
+			end
+			Result[63:32] = 31'b0;
+	end
 endmodule 
